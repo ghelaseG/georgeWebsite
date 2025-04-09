031abc91800eca4115fe34c92487e2f98e1a0930
ghelaseG <ghelase.georgian@gmail.com>
2025-04-09 22:14:55 +0100

    trying to add transition

--- js/transition.js
+++ js/transition.js
@@ -1,1 +1,11 @@
-
+if (document.querySelector(".hero h1")) {
+    const heroText = new SplitType(".hero h1", { types: "chars" });
+    gsap.set(heroText.chars, { y: 400 });
+    gsap.to(heroText.chars, {
+        y: 0,
+        duration: 1,
+        stagger: 0.075,
+        ease: "power4.out",
+        delay: 1,
+    });
+}
