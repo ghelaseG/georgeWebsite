707b3b1dbc55dbac94b903adfc35b8aead304036
ghelaseG <ghelase.georgian@gmail.com>
2025-04-10 08:38:05 +0100

    small retouch

--- js/script1.js
+++ js/script1.js
@@ -219,18 +219,6 @@ document.addEventListener("DOMContentLoaded", () => {
       ];
     }
     
-    if (document.querySelector(".hero h1")) {
-      const heroText = new SplitType(".hero h1", { types: "chars" });
-      gsap.set(heroText.chars, { y: 400 });
-      gsap.to(heroText.chars, {
-        y: 0,
-        duration: 1,
-        stagger: 0.075,
-        ease: "power4.out",
-        delay: 1,
-      });
-    }
-    
     shuffledHighlights.forEach((word) => {
       phase2.to(
         word,
