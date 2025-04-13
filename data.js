3c7a8ba6a3abe15749c7bad3338c7b5632453c24
ghelaseG <ghelase.georgian@gmail.com>
2025-04-13 03:45:24 +0100

    project page, new style etc

---
+++ data.js
@@ -0,0 +1,69 @@
+const data = [
+  {
+    "April 2023": [
+      {
+        img: "./assets/new/dashboard.png",
+        h1: "FTSE 100 Exploration",
+        copy: "In this exciting project, we delve into the world of data science and finance to unlock valuable insights from the FTSE 100 index.",
+        linkLabel: "Explore More",
+        linkSrc: "https://github.com/ghelaseG/data_science_project_finance",
+      },
+      {
+        img: "./assets/new/img2.jpg",
+        h1: "Fundamentals Data Science Bootcamp",
+        copy: "Discover the projects from the Bootcamp with HyperionDev",
+        linkLabel: "Explore More",
+        linkSrc: "https://github.com/ghelaseG/HyperionDev---Data-Science-Fundamentals-Tasks",
+      },
+    ],
+  },
+  {
+    "June 2023": [
+      {
+        img: "./assets/new/img5.jpg",
+        h1: "Final Capstone Bootcamp",
+        copy: "This repository contains a collection of projects focused on Natural Language Processing (NLP) and its applications.",
+        linkLabel: "Explore More",
+        linkSrc: "https://github.com/ghelaseG/finalCapstone",
+      },
+    ],
+  },
+  {
+    "March 2024": [
+      {
+        img: "./assets/new/img6.jpg",
+        h1: "Udemy",
+        copy: "10 real world applications",
+        linkLabel: "Explore More",
+        linkSrc: "https://github.com/ghelaseG/Udemy---The-Python-Mega-Course-Build-10-Real-World-Applications-by-Ardit-Sulce",
+      },
+      {
+        img: "./assets/new/img8.jpg",
+        h1: "Book",
+        copy: "Automate the Boring Stuff with Python",
+        linkLabel: "Check it Out",
+        linkSrc: "https://github.com/ghelaseG/Book---Automate-the-Boring-Stuff-with-Python-by-Al-Sweigart-",
+      },
+    ],
+  },
+  {
+    "April 2024": [
+      {
+        img: "./assets/new/img1.jpg",
+        h1: "Leetcode",
+        copy: "Python-Data-Structures-Algorithms-LEETCODE-Practice",
+        linkLabel: "Explore More",
+        linkSrc: "https://github.com/ghelaseG/Python-Data-Structures-Algorithms-LEETCODE-Exercises",
+      },
+      {
+        img: "./assets/new/img2.jpg",
+        h1: "Street Art Chronicles",
+        copy: "Delve into the vibrant world of street art with this project, showcasing the creativity and impact of urban artists who transform public spaces into canvases for expression.",
+        linkLabel: "Discover Now",
+        linkSrc: "https://example.com/street-art-chronicles",
+      },
+    ],
+  },
+];
+
+export default data;
