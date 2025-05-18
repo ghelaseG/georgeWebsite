1613c2fd35ced78ee217ce10b24ec0a497106133
ghelaseG <ghelase.georgian@gmail.com>
2025-05-18 16:30:49 +0100

    projects page, part 14

--- js/script1.js
+++ js/script1.js
@@ -1,7 +1,7 @@
 import data from "./data.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const activeColors = ["#5fa5f9", "#facc15", "#a78bfa", "#2cd4bf", "#23BEDA"];
+  const activeColors = ["#5fa5f9", "#f5e4a2", "#769eff", "#2cd4bf", "#23BEDA", "#8bfaa7"];
 
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
