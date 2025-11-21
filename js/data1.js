ec703d248022d9baedcf3265392a2d47cf9f057a
ghelaseG <ghelase.georgian@gmail.com>
2025-11-21 21:58:31 +0000

    small changes,p3

--- js/data1.js
+++ js/data1.js
@@ -1,6 +1,6 @@
 export const galleryItems = [
     {
-    copy: "On this page, you'll find a variety of materials, including textbooks, online courses, bootcamps, and documentaries that have significantly contributed to my understanding of data science. Additionally, I have included information about my qualifications, outlining the programs I completed and the skills I acquired throughout my academic journey. This should provide insight into both my educational background and my approach to studying data.",
+    copy: "On this page, I have included information about my qualifications, outlining the programs I completed and the skills I acquired throughout my academic journey.",
     },
     {
     title: "Northumbria University",
@@ -33,62 +33,6 @@ export const galleryItems = [
     period: "March 2023 - July 2023",
     type: "Bootcamp",
     course: "Data Science"
-    },
-    {
-    title: "Udemy",
-    place: "Remote",
-    period: "June 2022 - Ongoing",
-    copy: "Throughout these courses, I have gained essential skills in using Python for machine learning and programming. I learned to apply Python in hands-on projects and practical tasks. Learning to code is made enjoyable by these courses, which effectively simplify complex concepts.",
-    type: "Online Course",
-    course: "Python for Data Science and Machine Learning Bootcamp, The Complete Python Bootcamp From Zero to Hero in Python, Python Data Structures & Algorithms (Leetcode), The Python Mega Course: Build 10 Real World Applications"
-    },
-    {
-    title: "Books",
-    copy: "The first book dives into the most popular strategies that data scientists at leading companies use. It’s a fantastic guide that shows you how to think creatively with data science to tackle challenges in all sorts of fields. The second book is a wonderful resource for anyone looking to learn R for Data Science. I’ve already made it through half of the book, and I feel really confident in using R to solve problems now.",
-    place: "-",
-    period: "-",
-    type: "Read",
-    course: "Data Science: Dive Into Data Science: Use Python To Tackle Your Toughest Business Challenges, R for Data Science: Import, Tidy, Transform, Visualize, and Model Data",
-    },
-    {
-    title: "Books",
-    copy: "The first two books on statistics have become my all-time favorites. I read the first book only once, but it made such a lasting impression that I still recall most of the key concepts and insights from it. The second book came highly recommended by a senior data scientist (a big thank you for that!), and after listening to it just once, I found it so engaging and informative that I can’t wait to immerse myself in it again.",
-    place: "-",
-    period: "-",
-    type: "Read / Audio",
-    course: "Statistics: The Art of Statistics: Learning from Data, The Drunkard's Walk: How Randomness Rules Our Lives, Statistics Without Tears, How to Lie with Statistics",
-    },
-    {
-    title: "Books",
-    copy: "'Automate the Boring Stuff with Python, 2nd Edition' was my first step into the world of Python programming. I clearly remember how it took me several months to read through it, yet every moment spent was rewarding. This book not only taught me the fundamentals of coding but also sparked my enthusiasm for programming as a powerful tool for problem-solving.",
-    place: "-",
-    period: "-",
-    type: "Read / Audio",
-    course: "Python: Automate the Boring Stuff with Python, 2nd Edition; Grokking Algorithms, The Pragmatic Programmer: Your Journey to Mastery, Hello World: How to Be Human in the Age of the Machine",
-    },
-    {
-    title: "Books",
-    copy: "Such a great book written by Josh Starmer! If you struggle to understand how machine learning works, this book is an excellent resource that explains what is happening behind the scenes, accompanied by fun illustrations. (Tip: He has a fantastic YouTube channel, too!)",
-    place: "-",
-    period: "-",
-    type: "Read",
-    course: "Machine Learning: The StatQuest Illustrated Guide To Machine Learning",
-    },
-    {
-    title: "Video",
-    copy: "This documentary was excellent for learning about William Farr, who, in the 1860s, used census data to determine the cause of cholera in Soho. It also highlighted Claude Shannon, who eliminated annoying background noise on telephone lines by converting speech into code.",
-    place: "-",
-    period: "-",
-    type: "Documentary",
-    course: "The Joy of Data | BBC Documentary",
-    },
-    {
-    title: "Video",
-    copy: "I discovered this course on the YouTube channel freeCodeCamp.org, which provides a clear explanation of computer science and programming.",
-    place: "-",
-    period: "-",
-    type: "Lecture",
-    course: "Harvard CS50 – Full Computer Science University Course",
     },
   ];
   
